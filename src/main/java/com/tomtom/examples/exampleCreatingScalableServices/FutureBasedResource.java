83ccd405a017773207f645aa8092ea7a60dc4d21
Rijn Buve <rijn@buve.nl>
2015-04-13 10:16:26 +0200

    Added root URL/favicon

--- src/main/java/com/tomtom/examples/exampleCreatingScalableServices/FutureBasedResource.java
+++ src/main/java/com/tomtom/examples/exampleCreatingScalableServices/FutureBasedResource.java
@@ -29,7 +29,7 @@ import javax.ws.rs.core.MediaType;
 
 /**
  * This example class provides all REST API calls with an Akka Futures based implementation.
- *
+ * <p>
  * Requests might in rare exception cases be answered with a HTTP 500 Internal Server Error.
  */
 @Path("/")
@@ -44,22 +44,29 @@ public interface FutureBasedResource extends SimpleThreadBasedResource {
 
     /**
      * Additional method: this method returns the current version of the application.
-     *
+     * <p>
      * Return HTTP status 200.
      *
      * @param response Version, {@link com.tomtom.examples.exampleCreatingScalableServices.binders.VersionBinder}.
      */
     @GET
     @Path("example/2/version")
-    void getVersion(
-            @Nonnull @Suspend(ApiConstants.SUSPEND_TIMEOUT) AsynchronousResponse response);
+    void getVersion(@Nonnull @Suspend(ApiConstants.SUSPEND_TIMEOUT) AsynchronousResponse response);
 
+    /**
+     * Add implementation of returning a favicon.
+     *
+     * @param response Favicon.
+     */
     @GET
     @Path("favicon.ico")
-    void getFavicon(
-            @Nonnull @Suspend(ApiConstants.SUSPEND_TIMEOUT) AsynchronousResponse response);
+    void getFavicon(@Nonnull @Suspend(ApiConstants.SUSPEND_TIMEOUT) AsynchronousResponse response);
 
+    /**
+     * Add implementation of returning something for the root URL.
+     *
+     * @param response Root URL response.
+     */
     @GET
-    void getRoot(
-            @Nonnull @Suspend(ApiConstants.SUSPEND_TIMEOUT) AsynchronousResponse response);
+    void getRoot(@Nonnull @Suspend(ApiConstants.SUSPEND_TIMEOUT) AsynchronousResponse response);
 }
