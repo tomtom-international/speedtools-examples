1b802283151376aa5277fd4451fd507d80ab0d13
Rijn Buve <rijn@buve.nl>
2015-05-09 18:29:20 +0200

    Added missing JAR files

--- gatling/user-files/simulations/safertools/SimpleTestRestApi.scala
+++ gatling/user-files/simulations/safertools/SimpleTestRestApi.scala
@@ -1,19 +1,3 @@
-/*
- *  Copyright (C) 2015. TomTom International BV (http://tomtom.com).
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
 package speedtools
 
 import com.excilys.ebi.gatling.core.Predef._
@@ -22,23 +6,23 @@ import assertions._
 
 class SimpleTestRestApi extends Simulation {
 
-  val httpConf = httpConfig.
-    baseURL("http://localhost:8080").
-    acceptCharsetHeader("utf-8").
-    acceptHeader("application/json").
-    acceptLanguageHeader("en-US").
-    disableFollowRedirect
+    val httpConf = httpConfig.
+        baseURL("http://localhost:8080").
+        acceptCharsetHeader("utf-8").
+        acceptHeader("application/json").
+        acceptLanguageHeader("en-US").
+        disableFollowRedirect
 
-  val headers = Map(
-    "Accept" -> "application/json")
+    val headers = Map(
+        "Accept" -> "application/json")
 
-  val scnWarmUp = scenario("Warm-Up").
-    exec(http("person").get("/example/1/person").headers(headers)).
-    exec(http("person").get("/example/2/person").headers(headers))
+    val scnWarmUp = scenario("Warm-Up").
+        exec(http("person").get("/example/1/person").headers(headers)).
+        exec(http("person").get("/example/2/person").headers(headers))
 
-  setUp(
-    scnWarmUp.users(1).ramp(1).protocolConfig(httpConf))
+    setUp(
+        scnWarmUp.users(1).ramp(1).protocolConfig(httpConf))
 
-  assertThat(
-    global.successfulRequests.percent.is(100))
+    assertThat(
+        global.successfulRequests.percent.is(100))
 }
