cd476f5c3c1a6c1d82ebabd60943746600ec011c
Rijn Buve <rijn@buve.nl>
2017-06-26 13:44:40 +0200

    Updated copyright, moved to SpeedTools 3.2.0.

--- src/main/java/com/tomtom/examples/exampleCreatingScalableServices/security/AuthenticationServiceImpl.java
+++ src/main/java/com/tomtom/examples/exampleCreatingScalableServices/security/AuthenticationServiceImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2017, Stichting Mapcode Foundation (http://www.mapcode.com)
+ * Copyright (C) 2012-2017. TomTom International BV (http://tomtom.com).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,25 +14,8 @@
  * limitations under the License.
  */
 
-/**
- * Copyright (C) 2017, TomTom International BV (http://www.tomtom.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.tomtom.examples.exampleCreatingScalableServices.security;
 
-
-import com.google.inject.Inject;
 import com.tomtom.speedtools.domain.Uid;
 import com.tomtom.speedtools.rest.security.AuthenticationService;
 import com.tomtom.speedtools.rest.security.Identity;
@@ -43,6 +26,7 @@ import scala.App;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import javax.inject.Inject;
 
 /**
  * This class is the implementation of the {@link AuthenticationService}. It contains the logic to
--- src/main/java/com/tomtom/examples/exampleCreatingScalableServices/security/SecurityInterceptorConfigurer.java
+++ src/main/java/com/tomtom/examples/exampleCreatingScalableServices/security/SecurityInterceptorConfigurer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2017, Stichting Mapcode Foundation (http://www.mapcode.com)
+ * Copyright (C) 2012-2017. TomTom International BV (http://tomtom.com).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,24 +14,8 @@
  * limitations under the License.
  */
 
-/**
- * Copyright (C) 2017, TomTom International BV (http://www.tomtom.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.tomtom.examples.exampleCreatingScalableServices.security;
 
-import com.google.inject.Inject;
 import com.tomtom.examples.exampleCreatingScalableServices.FutureBasedResource;
 import com.tomtom.examples.exampleCreatingScalableServices.SimpleThreadBasedResource;
 import com.tomtom.speedtools.rest.security.SecurityInterceptor;
@@ -39,6 +23,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
+import javax.inject.Inject;
 
 /**
  * This class configures the {@link SecurityInterceptor} to require ro disabled security for REST
